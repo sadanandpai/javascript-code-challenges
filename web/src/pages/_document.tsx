74395c50d21a559cff80f815624c55380acc1c5a
Sadanand Pai <sadanand.pai@coindcx.com>
2023-09-17 22:01:17 +0530

    Web: Analytics integration

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+import Script from "next/script";
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+            if (window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1') {
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            }
+            })(window, document, "clarity", "script", "iwnh67o45k");`,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
