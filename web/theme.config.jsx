1ce66668093852a73f32a5cfcaa5f6f4c821bf15
Sadanand Pai <sadanand.pai@coindcx.com>
2023-09-18 10:40:31 +0530

    Meta tags enhancements

--- web/theme.config.jsx
+++ web/theme.config.jsx
@@ -1,18 +1,41 @@
+const metaTags = (
+  <>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <meta
+      name="description"
+      content="Collection of modern interview code challenges on JavaScript suitable for Interviewees | Interviewers | Knowledge test | Practice"
+    />
+    <meta property="og:url" content="https://jscodechallenges.vercel.app/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="JavaScript Code Challenges" />
+    <meta
+      property="og:description"
+      content="Collection of modern interview code challenges on JavaScript suitable for Interviewees | Interviewers | Knowledge test | Practice"
+    />
+    <meta
+      property="og:image"
+      content="https://jscodechallenges.vercel.app/hero.svg"
+    />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="twitter:domain" content="jscodechallenges.vercel.app" />
+    <meta
+      property="twitter:url"
+      content="https://jscodechallenges.vercel.app/"
+    />
+    <meta name="twitter:title" content="JavaScript Code Challenges" />
+    <meta
+      name="twitter:description"
+      content="Collection of modern interview code challenges on JavaScript suitable for Interviewees | Interviewers | Knowledge test | Practice"
+    />
+    <meta
+      name="twitter:image"
+      content="https://jscodechallenges.vercel.app/hero.svg"
+    />
+  </>
+);
+
 const config = {
-  head: (
-    <>
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-      <meta property="og:title" content="JavaScript Code Challenges" />
-      <meta
-        property="og:description"
-        content="Collection of modern interview code challenges on JavaScript suitable for Interviewees | Interviewers | Knowledge test | Practice"
-      />
-      <meta
-        property="og:image"
-        content="https://jscodechallenges.vercel.app/hero.svg"
-      />
-    </>
-  ),
+  head: metaTags,
   logo: <span>JavaScript Code Challenges</span>,
   project: {
     link: "https://github.com/sadanandpai/javascript-code-challenges/",
